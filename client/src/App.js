75979953e3e191809c769439cd2630aeb61e90ae
Matthew DeFusco <matthewdefusco@Matthews-MacBook-Pro.local>
2023-03-08 04:51:58 -0500

    testing out my first commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
-
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>hello!</h1>
+      <h2>This is Matt's addition for a test commit!</h2>
     </div>
   );
 }
