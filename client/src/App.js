1247fea4c78728bedd97eaf87d3f72c4c50977f1
Fabian Juren <fjuren@gmail.com>
2023-03-08 08:27:36 -0800

    Add form for user input field & submit button

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>hello!</h1>
       <h2>This is Matt's addition for a test commit!</h2>
-      <p>One more change</p>
+      <div id="title">
+        <h1>Simple to-do app</h1>
+      </div>
+      <div id="add-item-form">
+        <form method="POST">
+          <input type="text" id="todo-item"></input>
+          <button type="submit" id="submit-btn">
+            + Add
+          </button>
+        </form>
+      </div>
+      <div>{/* add to-do item component here? */}</div>
     </div>
   );
 }
