54485fd824925d0087190407fa7819a51afb8a9d
Fabian Juren <fjuren@gmail.com>
2023-03-08 17:14:50 -0800

    Add controller folder and created callback to handle todo_posts with validation & sanitization using express-validator. Updated front-end form with new label

--- server/routes/todo.js
+++ server/routes/todo.js
@@ -1,32 +1,21 @@
-const express = require('express');
-const db = require('../db/connect.js');
+const express = require("express");
+const todoController = require("../controllers/todoController");
+const db = require("../db/connect.js");
 
 const router = express.Router();
 // Client access: NOTE - if you want to interact with the database,
 // you first need to get a reference to the collection (think of a table in SQL).
 // To do this, you have to await the return of calling client.db().collection('todos') and then you can do what you need off the
 // collection - see the example below in the POST request handler.
-const client = db.getClient();
 
 // Create a route so that when a POST request is made, it stores the request body in the database under a collection called "todo".
-router.post('/todo', async (req, res, next) => {
-  console.log(req.body);
-  // Get access to the collection (basically the same concept as a table in the SQL world).
-  let collection = await client.db().collection('todos');
-
-  // TODO: VALIDATE INCOMING REQUEST
-
-  // Wait to get a result back from inserting the request body content into the collection.
-  let result = await collection.insertOne(req.body);
-  // Send a response back to the client letting them know that the new entry was added to the db (this should be updated).
-  res.send(result).status(204);
-});
+router.post("/", todoController.todo_post);
 
 // Create a route that will return all of the items within a collection.
-router.get('/todo', async (req, res, next) => {
+router.get("/todo", async (req, res, next) => {
   // TODO: IMPLEMENT THE REAL ROUTE HANDLER
   console.log(db);
-  res.send('Hello!');
+  res.send("Hello!");
 });
 
 module.exports = router;
