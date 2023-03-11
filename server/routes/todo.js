c3d1b094c5ee983f19074e2de73ec7bffc910b7f
Fabian Juren <fjuren@gmail.com>
2023-03-11 11:11:02 -0800

    Add backend change to update item as completed

--- server/routes/todo.js
+++ server/routes/todo.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const todoController = require('../controllers/todoController');
+const express = require("express");
+const todoController = require("../controllers/todoController");
 
 const router = express.Router();
 
 // Create a route so that when a POST request is made, it stores the request body in the database under a collection called "todo".
-router.post('/todo', todoController.todo_post);
+router.post("/todo", todoController.todo_post);
 
 // Create a route that will return all of the items within a collection.
-router.get('/todo', todoController.todo_get);
+router.get("/todo", todoController.todo_get);
 
 // Create a route to mark item as complete (we could also use this if we wanted to mark the item as not complete)
-router.put('/todo/item/:id', todoController.todo_complete);
+router.put("/todo/:id", todoController.todo_complete);
 
 module.exports = router;
