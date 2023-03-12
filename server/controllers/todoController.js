30afcf6f9ac56da036ef3799b98841b039a366fb
Fabian Juren <fjuren@gmail.com>
2023-03-11 17:06:38 -0800

    Completed todo_delete controller to delete todo item from db. Page still requires refresh though so we need to rerender the page on front end

--- server/controllers/todoController.js
+++ server/controllers/todoController.js
@@ -80,5 +80,21 @@ exports.todo_complete = async (req, res, next) => {
 
 exports.todo_delete = async (req, res, next) => {
   // pass req.params.id to mongodb findOneAndDelete to delete the item
-  console.log(`ID ${req.params.id}`);
+  const oID = new ObjectId(req.params.id);
+  try {
+    let collection = await client.db().collection("todos");
+    let results = await collection.findOneAndDelete(
+      { _id: oID },
+      function (err, doc) {
+        if (err) {
+          throw err;
+        } else {
+          console.log("deleted");
+        }
+      }
+    );
+  } catch (err) {
+    console.log(err);
+    res.send(err).status(500);
+  }
 };
